4d095782924804714eaa8e03e6ee5fa5ef58f1a3
Muhammad Haris <muhammad.haris@braehler-systems.com>
2023-06-23 12:08:09 +0200

    Text utils react app for text formatting and manipulation

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import "./App.css";
+import React, { useState } from "react";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  //Link,
+} from "react-router-dom";
+
+function App() {
+  const [alert, setAlert] = useState();
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    const htmlElement = document.documentElement;
+    if (htmlElement.hasAttribute("data-bs-theme")) {
+      htmlElement.removeAttribute("data-bs-theme");
+    } else {
+      htmlElement.setAttribute("data-bs-theme", "dark");
+      showAlert("Dark mode has been enabled", "success");
+    }
+  };
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar title="Text Utils" toggleMode={toggleMode} />
+        <div className="container my-3">
+          <Alert alert={alert} />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the text to analyze below"
+                />
+              }
+            />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+
+export default function TextForm(props) {
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleRmSpacesClick = () => {
+        let newText = text.replace(/\s+/g, ' ').trim()
+        setText(newText);
+    }
+
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText(text);
+    }
+
+    const handleClearClick = () => {
+        props.showAlert("You are going to clear all the text", "danger");
+        let newText = '';
+        setText(newText);
+    }
+
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const [text, setText] = useState('');
+    return (
+        <>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-outline-primary mx-1 my-1" onClick={handleUpClick}>To Uppercase</button>
+                <button className="btn btn-outline-secondary mx-1 my-1" onClick={handleLoClick}>To Lowercase</button>
+                <button className="btn btn-outline-danger mx-1 my-1" onClick={handleClearClick}>Clear</button>
+                <button className="btn btn-outline-success mx-1 my-1" onClick={handleCopyClick}>Copy</button>
+                <button className="btn btn-outline-info mx-1 my-1" onClick={handleRmSpacesClick}>Remove Extra Spaces</button>
+            </div>
+            <div className="container my-5">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length - 1} words and {text.length} characters</p>
+                <p>{0.008 * (text.split(" ").length - 1)} Minutes read</p>
+                <h3>Preview</h3>
+                <p>{text.length>0? text:"Enter something in the text box to preview it"}</p>
+            </div>
+        </>
+    );
+}
